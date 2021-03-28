fc99d255c30230e6fd11309d13d47ddaae49fe63
Idhrendur <idhrendur@hotmail.com>
2021-03-27 17:38:23 -0700

    ReSharper

--- Source/main.cpp
+++ Source/main.cpp
@@ -6,23 +6,29 @@ constexpr int imageHeight = 256;
 
 int main()
 {
-	std::cout << "P3\n";
-	std::cout << imageWidth << ' ' << imageHeight << "\n";
-	std::cout << "255\n";
-	for (int j = imageHeight - 1; j >= 0; --j)
+	try
 	{
-		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-		for (int i = 0; i < imageWidth; ++i)
+		std::cout << "P3\n";
+		std::cout << imageWidth << ' ' << imageHeight << "\n";
+		std::cout << "255\n";
+		for (auto j = imageHeight - 1; j >= 0; --j)
 		{
-			const auto r = static_cast<float>(i) / (imageWidth - 1);
-			const auto g = static_cast<float>(j) / (imageHeight - 1);
-			const auto b = 0.25;
+			std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+			for (auto i = 0; i < imageWidth; ++i)
+			{
+				const auto r = static_cast<float>(i) / (imageWidth - 1);
+				const auto g = static_cast<float>(j) / (imageHeight - 1);
+				const auto b = 0.25;
 
-			const int ir = static_cast<int>(255.999 * r);
-			const int ig = static_cast<int>(255.999 * g);
-			const int ib = static_cast<int>(255.999 * b);
+				const auto ir = static_cast<int>(255.999F * r);
+				const auto ig = static_cast<int>(255.999F * g);
+				const auto ib = static_cast<int>(255.999F * b);
 
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+				std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			}
 		}
+	}
+	catch (...)
+	{
 	}
 }
